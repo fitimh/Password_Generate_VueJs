6db72eccfe86f038ef59f68a539913746468c0bc
Fitim Hoti <fitim.hoti100@gmail.com>
2022-01-25 23:23:32 +0100

    design form generate password
    design form generate password

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+   <GeneratePassword msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.min.css";
+import GeneratePassword from "./components/GeneratePassword.vue";
+// <link
+//    rel="stylesheet"
+//    href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+//    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+//    crossorigin="anonymous"
+// ></link>;
+
+export default {
+   name: "App",
+   components: {
+      GeneratePassword,
+   },
+};
+</script>
+
+<style></style>
